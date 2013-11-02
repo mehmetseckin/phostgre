a7cca3c5a8263b89c9ffb6d0417d708a4ee48c17
seckin92 <>
2013-11-02 15:57:39 +0200

    NetBeans plugin test commit

---
+++ src/phostgre.php
@@ -0,0 +1,9 @@
+<?php
+ /**
+  * Phostgre is a collection of classes that provides connection and some CRUD operations between PHP and PostgreSQL databases.
+  *
+  * @author: Mehmet Seckin
+  * @email : seckin92@gmail.com
+  * @type  : Library
+  */
+?>
