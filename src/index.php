4ab4d20e4d07cc85778faf0c86041f07645ff992
Mehmet Seçkin <info@mehmetseckin.com>
2013-12-03 13:42:34 +0200

    Minor optimizations

--- src/index.php
+++ src/index.php
@@ -1,14 +1,18 @@
 <?php
-
+// Initialize script
 require_once "load.php";
+
+// Initialize Loggy. Visit http://github.com/seckin92/loggy
+// for more information.
+include "loggy/load.php";
 
 $loggy = new Loggy();
 
 $engine = new Engine();
 
-$name = "Mehmet Seckin";
-$salary = 5622;
-$hired = "2013-12-20";
+$name = "John Lunn";
+$salary = 4250;
+$hired = "2012-12-07";
 
 $engine->addEmployee($name, $salary, $hired);
 $result = $engine->loadBoolean();
--- src/load.php
+++ src/load.php
@@ -4,19 +4,14 @@ error_reporting(E_ALL);
 
 // Database settings ...
 define("PgSQL_HOSTNAME", "localhost");
-define("PgSQL_USERNAME", "phostgre");
-define("PgSQL_PASSWORD", "phostgre");
+define("PgSQL_USERNAME", "mku");
+define("PgSQL_PASSWORD", "mku2012");
 define("PgSQL_DATABASE", "phostgre_test");
 define("PgSQL_PORT"    , "5432");
 
 // Database Constants ...
 define("PgSQL_TRUE", "t");
 define("PgSQL_FALSE", "f");
-
-// Initialize Loggy. Visit http://github.com/seckin92/loggy
-// for more information.
-include "loggy/load.php";
-
 // Initialize the Engine.
 include "lib/engine.php";
 ?>
